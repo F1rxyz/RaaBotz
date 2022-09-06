5525a1e9f0283f2a3dbc1921ab81d3ab852374b2
F1rxyz <110138791+F1rxyz@users.noreply.github.com>
2022-09-06 10:13:58 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -31,17 +31,17 @@ module.exports = {
         global.img = 'https://telegra.ph/file/dffdaf0e8d59ad104e526.jpg'
         global.fla = pickRandom(global.flaaa)
         global.namabot = conn.user.name
-        global.packname = '© RaaBotz-MD'
-        global.author = 'Made By Claraa © 2022'
+        global.packname = '© ShellTear:3'
+        global.author = 'Made By Moonlight © 2022'
         global.packname2 = 'Made With'
-        global.author2 = 'Raa Bot By Claraa © 2022'
-        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Claraa˚'
-        global.wm2 = '©𝙍𝙖𝙖-𝘽𝙤𝙩𝙯-𝙈𝘿' + ' ' + 'By 𝐂𝐥𝐚𝐫𝐚𝐚'
-        global.wm = '© 𝙍𝙖𝙖-𝘽𝙤𝙩𝙯--𝙈𝘿'
+        global.author2 = 'ShellTear Bot by Moonlight © 2022'
+        global.wm3 = '·͟͟͟͟͟͟͞͞͞͞͞͞Made By Moonlight˚'
+        global.wm2 = '© ShellTear' + ' ' + 'By Moonlight'
+        global.wm = '© Moonlight '
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'By Claraa'
+        global.colong2 = 'By Moonlight'
         global.kontak2 = [
-        ['62895604670507', 'Claraa', 'Creator Bot', 'Clarz939@support.com', true],
+        ['6285806459088', 'Moonlight', 'Creator Bot', 'Clarz939@support.com', true],
         ['79217628068', 'Raa Botz-MD', 'Bot WhatsApp', 'Just Bot', true]
         ]
         global.bg = await (await fetch(img)).buffer()
